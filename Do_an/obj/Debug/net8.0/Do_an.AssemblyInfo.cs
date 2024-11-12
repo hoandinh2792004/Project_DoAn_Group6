da2786d94e09303a3605d4fab58171b5dbb48e13
Hoan <hoandinh2792004@gmail.com>
2024-11-12 13:52:33 +0700

    before merge

--- Do_an/obj/Debug/net8.0/Do_an.AssemblyInfo.cs
+++ Do_an/obj/Debug/net8.0/Do_an.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do_an")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90aef4b264b268979d78d912a1006aca6a5c22d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77ec0daf256b10b9a736a67c3b02069515b1ca35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do_an")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do_an")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
