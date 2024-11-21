780751303f8505525e5aafb8b1b03f090632dc81
hungreview2004 <daohung133122@gmail.com>
2024-11-21 10:16:49 +0700

    Chatbot

---
+++ Do_an/Controllers/ChatSuport.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Do_an.Controllers
+{
+    public class ChatSuport : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Do_an/Controllers/Services.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Do_an.Controllers
+{
+    public class Services : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
