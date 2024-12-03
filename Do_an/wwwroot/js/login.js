5b3a707d62741c7946737adf04701147bdc71f42
pmduc1509 <152267640+pmduc1509@users.noreply.github.com>
2024-12-03 20:39:42 +0700

    Update login.js

--- Do_an/wwwroot/js/login.js
+++ Do_an/wwwroot/js/login.js
@@ -1,4 +1,4 @@
-﻿const container = document.getElementById('container');
+const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
